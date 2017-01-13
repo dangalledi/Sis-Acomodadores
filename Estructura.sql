c2582cb5856f6ee507da4151e4e34e142537bd96
Patricio Pérez <pperez@boaboa.org>
2017-01-13 14:33:42 -0300

    CHG: Modificar estructura sql
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

--- Estructura.sql
+++ Estructura.sql
@@ -1,37 +1,27 @@
 start transaction;
-drop table if exists ADMIN CASCADE;
-drop table if exists FUNCION CASCADE;
-drop table if exists ACOMOD CASCADE;
-drop table if exists PARTICIPA CASCADE;
+drop table if exists users CASCADE;
+drop table if exists funciones CASCADE;
+drop table if exists participantes_funcion CASCADE;
 
-create table ADMIN (
+create table users (
 	id SERIAL NOT NULL,
 	rut INT NOT NULL,
-	pass VARCHAR(10) NOT NULL,
+	password VARCHAR(10) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 
-create table FUNCION (
+create table funciones (
 	id SERIAL NOT NULL,
-	fecha DATE NOT NULL,
-	hora VARCHAR (10) NOT NULL,
-	admin_id INT REFERENCES ADMIN (id) NOT NULL,
+	fecha timestamp NOT NULL,
+	admin_id INT REFERENCES users (id) NOT NULL,
 	PRIMARY KEY (id)
 );
 
-
-create table ACOMOD (
+create table participantes_funcion (
 	id SERIAL NOT NULL,
-	rut INT NOT NULL,
-	pass VARCHAR (10) NOT NULL,
-	PRIMARY KEY (id)
-);
-
-create table PARTICIPA (
-	id SERIAL NOT NULL,
-	funcion_id INT REFERENCES FUNCION (id) NOT NULL,
-	acomod_id INT REFERENCES ACOMOD (id) NOT NULL,
+	funcion_id INT REFERENCES funciones (id) NOT NULL,
+	acomodador_id INT REFERENCES users (id) NOT NULL,
 	PRIMARY KEY (id)
 );
 
