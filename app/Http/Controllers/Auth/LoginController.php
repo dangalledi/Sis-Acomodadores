d97aba1ff552604c3e6f342fee39479e42631c31
Patricio Pérez <pperez@boaboa.org>
2017-02-23 17:01:59 -0300

    CHG: Usar login con mail
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 use UTEM\Utils\Rut;
 
@@ -46,7 +47,7 @@ class LoginController extends Controller
      */
     public function username()
     {
-        return 'rut';
+        return 'email';
     }
 
     /**
