f5389c9d794b2793759ed254b56fda1eae137d5a
Daniela Galleguillos <dangalledi@gmail.com>
2017-02-23 20:11:15 -0300

    Agrega formulario creacion funciones
    Signed-off-by: Daniela Galleguillos <dangalledi@gmail.com>

---
+++ resources/views/funciones/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <form method="post" action="{{ url('funciones') }}">
+        {{csrf_field()}}
+        <div class="form-group">
+          <label for="fecha">fecha</label>
+          <input type="text" name='fecha' class="form-control" id="fecha" placeholder="fecha de la funcion">
+        </div>
+        <div class="form-group">
+          <label for="acomodadores">acomodadores</label>
+          <input type="text" name='acomodadores' class="form-control" id="acomodadores" placeholder="numero de acomodadores">
+        </div>
+        <div class="form-group">
+          <label for="comentario">comentario</label>
+          <textarea class="form-control" name="comentario" id="comentario" placeholder="agregar comentario"></textarea>
+        </div>
+        <button type="submit" class="btn btn-default">agregar funcion</button>
+    </form>
+  </div>
+</div>
+@endsection
